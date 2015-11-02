e06ad80f37486b23c1e1eb7aad16de787ccbb230
Dominik <domin1991@gmail.com>
2015-11-02 21:41:09 +0100

    Simple Ruler

---
+++ DwachWPF.Controls/Ruler.Properties.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace DwachWPF.Controls
+{
+    public partial class Ruler
+    {
+        public decimal Start
+        {
+            get { return (decimal)GetValue(StartProperty); }
+            set { SetValue(StartProperty, value); }
+        }
+        
+        public static readonly DependencyProperty StartProperty =
+            DependencyProperty.Register("Start", typeof(decimal), typeof(Ruler), new FrameworkPropertyMetadata(0.0m, FrameworkPropertyMetadataOptions.AffectsRender));
+
+
+
+        public decimal Stop
+        {
+            get { return (decimal)GetValue(StopProperty); }
+            set { SetValue(StopProperty, value); }
+        }
+
+        public static readonly DependencyProperty StopProperty =
+            DependencyProperty.Register("Stop", typeof(decimal), typeof(Ruler), new FrameworkPropertyMetadata(100.0m, FrameworkPropertyMetadataOptions.AffectsRender));
+
+
+
+        public decimal Step
+        {
+            get { return (Decimal)GetValue(StepProperty); }
+            set { SetValue(StepProperty, value); }
+        }
+        
+        public static readonly DependencyProperty StepProperty =
+            DependencyProperty.Register("Step", typeof(decimal), typeof(Ruler), new FrameworkPropertyMetadata(10.0m, FrameworkPropertyMetadataOptions.AffectsRender));
+
+
+        public decimal SmallStep
+        {
+            get { return (decimal)GetValue(SmallStepProperty); }
+            set { SetValue(SmallStepProperty, value); }
+        }
+        
+        public static readonly DependencyProperty SmallStepProperty =
+            DependencyProperty.Register("SmallStep", typeof(decimal), typeof(Ruler), new FrameworkPropertyMetadata(2.0m, FrameworkPropertyMetadataOptions.AffectsRender));
+
+
+
+        public Brush Brush
+        {
+            get { return (Brush)GetValue(BrushProperty); }
+            set { SetValue(BrushProperty, value); }
+        }
+        
+        public static readonly DependencyProperty BrushProperty =
+            DependencyProperty.Register("Brush", typeof(Brush), typeof(Ruler), new FrameworkPropertyMetadata(Brushes.Black, FrameworkPropertyMetadataOptions.AffectsRender));
+
+
+        public Brush IndycatorBrush
+        {
+            get { return (Brush)GetValue(IndycatorBrushProperty); }
+            set { SetValue(IndycatorBrushProperty, value); }
+        }
+        
+        public static readonly DependencyProperty IndycatorBrushProperty =
+            DependencyProperty.Register("IndycatorBrush", typeof(Brush), typeof(Ruler), new FrameworkPropertyMetadata(Brushes.Red, FrameworkPropertyMetadataOptions.AffectsRender));
+    }
+}
---
+++ DwachWPF.Controls/Ruler.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace DwachWPF.Controls
+{
+    public partial class Ruler : FrameworkElement
+    {
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+
+            Pen borderPen = new Pen(Brush, 1.0);
+            Pen pen = new Pen(Brush, 1.0);
+
+            drawingContext.DrawRectangle(null, borderPen, new Rect(new Point(0.0, 0.0), new Point(ActualWidth, ActualHeight)));
+            DrawSmallSteps(drawingContext, pen);
+            DrawSteps(drawingContext, pen);
+        }
+
+        private void DrawSmallSteps(DrawingContext drawingContext, Pen pen)
+        {
+            for (decimal i = Start; i < Stop; i += SmallStep)
+            {
+                double x = ((double)((i - Start) / (Stop - Start)) * ActualWidth);
+
+                drawingContext.DrawLine(pen, new Point(x, ActualHeight), new Point(x, ActualHeight * 0.8));
+            }
+        }
+
+        private void DrawSteps(DrawingContext drawingContext, Pen pen)
+        {
+            for (decimal i = Start; i < Stop; i += Step)
+            {
+                double x = ((double)((i - Start) / (Stop - Start)) * ActualWidth);
+
+                drawingContext.DrawLine(pen, new Point(x, ActualHeight), new Point(x, ActualHeight * 0.6));
+
+                FormattedText value = new FormattedText(i.ToString(CultureInfo.CurrentCulture),
+                            CultureInfo.CurrentCulture,
+                            FlowDirection.LeftToRight,
+                            new Typeface("Arial"),
+                            PtToDip((int)(ActualHeight / 3)),
+                            Brush);
+                value.SetFontWeight(FontWeights.Regular);
+                value.TextAlignment = TextAlignment.Center;
+
+                drawingContext.DrawText(value, new Point(x, ActualHeight * 0.1));
+            }
+        }
+
+        private double PtToDip(int pt)
+        {
+            return (pt * 96.0 / 72.0);
+        }
+        
+    }
+}
